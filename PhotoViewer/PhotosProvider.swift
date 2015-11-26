4abb3a484d812525560df7ee784b43eb51796537
sashablokhin <sashablokhindev@gmail.com>
2015-11-26 15:05:55 +0300

    Added Protocols

---
+++ PhotoViewer/PhotosProvider.swift
@@ -0,0 +1,29 @@
+//
+//  PhotosProvider.swift
+//  PhotoViewer
+//
+//  Created by Alexander Blokhin on 26.11.15.
+//  Copyright © 2015 Alexander Blokhin. All rights reserved.
+//
+
+import UIKit
+
+class PhotosProvider: NSObject {
+    
+    let photos: [Photo] = {
+        
+        var mutablePhotos: [Photo] = []
+        
+        for photoIndex in 0..<4 {
+            
+            let image = UIImage(named: "img_0\(photoIndex)")
+            
+            let title = NSAttributedString(string: "\(photoIndex + 1)", attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
+            let photo = Photo(image: image, attributedCaptionTitle: title)
+            
+            mutablePhotos.append(photo)
+        }
+        
+        return mutablePhotos
+    }()
+}
